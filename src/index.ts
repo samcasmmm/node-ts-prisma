9121c8f405a6743fd0fe727ee10ac07dfbb884a1
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-12 14:35:17 +0530

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Application } from 'express';
-import connectDatabase from '@/config/connectDatabase';
-import 'module-alias/register';
+import connectDatabase from './config/connectDatabase.js';
 
 const app: Application = express();
 
@@ -17,7 +16,7 @@ app.get('/', (req, res) => {
 const startServer = () => {
   app.listen(9001, () => {
     console.log('working');
-    // connectDatabase();
+    connectDatabase();
     console.log(`Server listening on http://localhost:${9001}`);
   });
 };
