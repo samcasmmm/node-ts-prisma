46cf406466d71b669f0ca8c51802c16c1c36d272
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-12-21 20:40:07 +0530

    revamping

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import express from 'express'
+import morgan from 'morgan';
 
 
 const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+   res.json({ msg: 'Hello' })
+})
+
+
+app.get('/template', (req, res) => {
+   const { uid, pid, tid } = req.query;
+
+   if (uid === undefined || pid === undefined || tid === undefined) {
+      res.status(400).json({ error: 'Please provide all parameters' });
+   } else {
+      res.json({ UserId: uid, PropertyId: pid, TemplateId: tid })
+
+   }
+});
+
+
 
 const server = () => {
    const port = 3000;
 
    app.listen(port, () => {
-      console.log(`http://localhost:7000`);
+      console.log(`Runing on http://localhost:${port}`);
    });
 };
 
