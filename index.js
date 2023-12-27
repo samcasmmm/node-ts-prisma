c689c33e5c7b2d6627ab5c48e00085527ac29490
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-12-27 20:55:18 +0530

    added redis

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config()
 // @ Imported File
 import app from './src/app.js'
 import { connectDB } from './src/config/index.js';
+import RedisClient from './src/config/redis.js';
 
 
 
@@ -22,6 +23,8 @@ const startServer = async () => {
       console.log(error);
    }
    app.listen(process.env.PORT, () => {
+      RedisClient.hSet('name', 'sameer bagwan').then(res => console.log(res)).catch(e => console.log(e))
+      console.log()
       console.log(
          `${process.env.NODE_ENV} server on http://localhost:${process.env.PORT}`
       );
